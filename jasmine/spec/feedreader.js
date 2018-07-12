c58407bd9c6073cb659deecc15422dbe45e60bd5
Matt Scott <mms.shpd@gmail.com>
2018-07-11 20:16:59 -0400

    added initial entries test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,39 +55,63 @@ $(function () {
 
     /* Write a new test suite named "The menu" */
     describe('The menu', function () {
-
-
+        const bodyTag = document.getElementsByTagName("BODY");
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('menu hidden', function () {
+            expect(bodyTag[0].className).toBe('menu-hidden');
+        });
 
         /* TODO: Write a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
+        it('menu changes', function () {
+            const menuIcon = $('.menu-icon-link');
+
+            menuIcon.click();
+            expect(bodyTag[0].className).not.toBe('menu-hidden');
+
+            menuIcon.click();
+            expect(bodyTag[0].className).toBe('menu-hidden');
+        });
     });
+
 
     /* Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function () {
 
-
-
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        const id = 0;
+
+        beforeEach(function (done) {
+            loadFeed(id, function () {
+                done();
+            });
+        });
+
+        it('at least a single entry in feed container', function (done) {
+            const articles = $('.feed').find('.entry');
+            console.log(articles);
+            expect(articles).toBeDefined();
+            done();
+        });
+
+
     });
 
     /* Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function () {
-
-
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
